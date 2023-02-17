bc3eaaa7e3c396a1ee7d19b9b8c7f5ebd84904ca
PRINCE46815 <sibusisoprince69@gmail.com>
2023-02-16 19:22:47 -0800

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.header>
+/**
+ * main - using the puts fuction to write a program that prints a line
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+puts("\"a multilingual puzzle is the same as programming
+");
+return (0);
+}
